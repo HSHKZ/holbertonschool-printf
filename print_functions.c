cf2dd366e0635cf9cc28bf27529cc12d39a530de
HSHKZ <lucasolegniel@gmail.com>
2024-07-29 11:24:41 +0200

    print_functions.c

---
+++ print_functions.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * print_char - Prints a character
+ * @args: Arguments list
+ * Return: Number of characters printed
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (_putchar(c));
+}
+
+/**
+ * print_string - Prints a string
+ * @args: Arguments list
+ * Return: Number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int i;
+
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	return (i);
+}
